dc884617c243993c0e615b12b9da41ac6872b738
melsayed8450 <137142724+mohamedCisapp@users.noreply.github.com>
2023-10-05 17:04:05 +0300

    first commit

---
+++ index.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+import pyqtgraph as pg
+
+uiclass, baseclass = pg.Qt.loadUiType("main.ui")
+
+class MainWindow(uiclass, baseclass):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
