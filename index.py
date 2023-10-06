612b7b4b8f5f66908b1790bc0389454f7fbd4550
Osama Mohamed Badawi <osama_badawi@outlook.com>
2023-10-06 16:48:48 +0300

    added two real-time functions for the two channels

--- index.py
+++ index.py
@@ -22,18 +22,21 @@ class MainWindow(uiclass, baseclass):
         # import signal to ch2
         self.import_signal_ch2.triggered.connect(self.import_signal_channel_2)
         # set timer for real-time plotting
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.update_plot)
-        self.is_plotting = False  # Flag to control real-time plotting
+        self.timer_1 = QTimer(self)
+        self.timer_1.timeout.connect(self.update_plot_1)
+        self.is_plotting_1 = False  # Flag to control real-time plotting
+        self.timer_2 = QTimer(self)
+        self.timer_2.timeout.connect(self.update_plot_2)
+        self.is_plotting_2 = False  # Flag to control real-time plotting
 
 
     def import_signal_channel_1(self):
         signal: Signal = self.get_signal_from_file()
-        self.render_signal_to_channel(channel=self.widget, signal=signal)
+        self.render_signal_to_channel_1(channel=self.widget, signal=signal)
     
     def import_signal_channel_2(self):
         signal: Signal = self.get_signal_from_file()
-        self.render_signal_to_channel(channel=self.widget_2, signal=signal)
+        self.render_signal_to_channel_2(channel=self.widget_2, signal=signal)
 
     def get_signal_from_file(self):
         # get path of signal files only of types (xls, csv, txt)
@@ -56,31 +59,58 @@ class MainWindow(uiclass, baseclass):
         signal: Signal = loader.load(file_path)
         return signal
 
-    def render_signal_to_channel(self, channel, signal):
+    def render_signal_to_channel_1(self, channel, signal):
         # Set up the initial plot
         pen = pg.mkPen(color=signal.color.value)
-        curve = channel.plot(signal.x_vec, signal.y_vec, pen=pen)
+        curve_1 = channel.plot(signal.x_vec, signal.y_vec, pen=pen)
 
         # Store the data and curve for real-time updating
-        self.x_vec = signal.x_vec
-        self.y_vec = signal.y_vec
-        self.curve = curve
-        self.data_index = 0
+        self.x_vec_1 = signal.x_vec
+        self.y_vec_1 = signal.y_vec
+        self.curve_1 = curve_1
+        self.data_index_1 = 0
 
         # Start the real-time plot
-        self.is_plotting = True
-        self.timer.start(1)  # Update every 1 ms
+        self.is_plotting_1 = True
+        self.timer_1.start(1)  # Update every 1 ms
 
-    def update_plot(self):
-       if self.is_plotting:
-            if self.data_index < len(self.x_vec):
-                x_data = self.x_vec[:self.data_index + 1]
-                y_data = self.y_vec[:self.data_index + 1]
-                self.curve.setData(x_data, y_data)
-                self.data_index += 1
+    def render_signal_to_channel_2(self, channel, signal):
+        # Set up the initial plot
+        pen = pg.mkPen(color=signal.color.value)
+        curve_2 = channel.plot(signal.x_vec, signal.y_vec, pen=pen)
+
+        # Store the data and curve for real-time updating
+        self.x_vec_2 = signal.x_vec
+        self.y_vec_2 = signal.y_vec
+        self.curve_2 = curve_2
+        self.data_index_2 = 0
+
+        # Start the real-time plot
+        self.is_plotting_2 = True
+        self.timer_2.start(1)  # Update every 1 ms
+
+    def update_plot_1(self):
+       if self.is_plotting_1:
+            if self.data_index_1 < len(self.x_vec_1):
+                x_data = self.x_vec_1[:self.data_index_1 + 1]
+                y_data = self.y_vec_1[:self.data_index_1 + 1]
+                self.curve_1.setData(x_data, y_data)
+                self.data_index_1 += 1
             else:
-                self.is_plotting = False
-                self.timer.stop()  # Stop the QTimer when all data is plotted untill we know what to do when finished
+                self.is_plotting_1 = False
+                self.timer_1.stop()  # Stop the QTimer when all data is plotted untill we know what to do when finished
+
+    def update_plot_2(self):
+       if self.is_plotting_2:
+            if self.data_index_2 < len(self.x_vec_2):
+                x_data = self.x_vec_2[:self.data_index_2 + 1]
+                y_data = self.y_vec_2[:self.data_index_2 + 1]
+                self.curve_2.setData(x_data, y_data)
+                self.data_index_2 += 1
+            else:
+                self.is_plotting_2 = False
+                self.timer_2.stop()  # Stop the QTimer when all data is plotted untill we know what to do when finished
+        
 
 
 def main():
