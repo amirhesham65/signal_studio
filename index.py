75ce19922ce212ebafa697c62907989b4101c8c6
Osama Mohamed Badawi <osama_badawi@outlook.com>
2023-10-06 16:17:27 +0300

    Real-time plotting

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt6 import QtCore, QtGui, QtWidgets
 from PyQt6.QtWidgets import QApplication, QMessageBox
+from PyQt6.QtCore import QTimer
 import pyqtgraph as pg
 import pandas as pd
 from pyqtgraph import PlotWidget
@@ -20,6 +21,11 @@ class MainWindow(uiclass, baseclass):
         self.import_signal_ch1.triggered.connect(self.import_signal_channel_1)
         # import signal to ch2
         self.import_signal_ch2.triggered.connect(self.import_signal_channel_2)
+        # set timer for real-time plotting
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_plot)
+        self.is_plotting = False  # Flag to control real-time plotting
+
 
     def import_signal_channel_1(self):
         signal: Signal = self.get_signal_from_file()
@@ -50,9 +56,31 @@ class MainWindow(uiclass, baseclass):
         signal: Signal = loader.load(file_path)
         return signal
 
-    def render_signal_to_channel(self, channel, signal: Signal) -> None:
+    def render_signal_to_channel(self, channel, signal):
+        # Set up the initial plot
         pen = pg.mkPen(color=signal.color.value)
-        channel.plot(signal.x_vec, signal.y_vec, pen=pen)
+        curve = channel.plot(signal.x_vec, signal.y_vec, pen=pen)
+
+        # Store the data and curve for real-time updating
+        self.x_vec = signal.x_vec
+        self.y_vec = signal.y_vec
+        self.curve = curve
+        self.data_index = 0
+
+        # Start the real-time plot
+        self.is_plotting = True
+        self.timer.start(1)  # Update every 1 ms
+
+    def update_plot(self):
+       if self.is_plotting:
+            if self.data_index < len(self.x_vec):
+                x_data = self.x_vec[:self.data_index + 1]
+                y_data = self.y_vec[:self.data_index + 1]
+                self.curve.setData(x_data, y_data)
+                self.data_index += 1
+            else:
+                self.is_plotting = False
+                self.timer.stop()  # Stop the QTimer when all data is plotted untill we know what to do when finished
 
 
 def main():
