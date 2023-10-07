7ce0f43fe5ca690f05cbee7abdbaef4057f94986
Amir Hesham Ibrahim <amirhesham65@gmail.com>
2023-10-07 12:11:26 +0300

    Refactor + Fix second slider

--- index.py
+++ index.py
@@ -1,13 +1,10 @@
-from math import floor
 import sys
-from PyQt6 import QtCore, QtGui, QtWidgets
-from PyQt6.QtWidgets import QApplication, QMessageBox
+from math import floor
+from PyQt6.QtWidgets import QApplication
 from PyQt6.QtCore import QTimer
 import pyqtgraph as pg
-import pandas as pd
-from pyqtgraph import PlotWidget
-from managers.signal_loader import ISignalLoader, TextSignalLoader, CSVSignalLoader, ExcelXSignalLoader, ExcelSignalLoader
 from models.signal import Signal
+from helpers.get_signal_from_file import get_signal_from_file
 
 uiclass, baseclass = pg.Qt.loadUiType("main.ui")
 
@@ -15,70 +12,56 @@ class MainWindow(uiclass, baseclass):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.handle_events()
-
-    def handle_events(self):
-        # import signal to ch1
-        self.import_signal_ch1.triggered.connect(self.import_signal_channel_1)
-        # import signal to ch2
-        self.import_signal_ch2.triggered.connect(self.import_signal_channel_2)
-        # set timer for real-time plotting
-        self.timer_1 = QTimer(self)
-        self.timer_1.timeout.connect(self.update_plot_1)
-        self.is_plotting_1 = False  # Flag to control real-time plotting
-        self.timer_2 = QTimer(self)
-        self.timer_2.timeout.connect(self.update_plot_2)
-        self.is_plotting_2 = False  # Flag to control real-time plotting
-        self.play_button_1.clicked.connect(self.play_pause_1)
-        self.speed_1 = 1     # Speed of channel 1
-        self.speed_button_1.clicked.connect(self.change_speed_1)
-        self.play_button_2.clicked.connect(self.play_pause_2)
-        self.speed_2 = 1     # Speed of channel 1
-        self.speed_button_2.clicked.connect(self.change_speed_2)
-        self.clear_button_1.clicked.connect(self.clear_1)
-        self.clear_button_2.clicked.connect(self.clear_2)
-        self.import_signal_ch2.triggered.connect(self.import_signal_channel_2)
-        self.actionPlay_Pause.triggered.connect(self.play_pause_1)
-        self.actionPlay_Pause_2.triggered.connect(self.play_pause_2)
+        self.initialize_state()
+        self.initialize_signals_slots()
 
         # Prevent zooming and paning
         self.widget.setMouseEnabled(x=False, y=False)
         self.widget_2.setMouseEnabled(x=False, y=False)
 
+    def initialize_state(self):
+        # Channel 1
+        self.timer_1 = QTimer(self)
+        self.is_plotting_1 = False
+        self.speed_1 = 1
+
+        # Channel 2
+        self.timer_2 = QTimer(self)
+        self.is_plotting_2 = False  
+        self.speed_2 = 1 
+
+    def initialize_signals_slots(self):
+        # Channel 1
+        self.import_signal_ch1.triggered.connect(self.import_signal_channel_1)
+        self.timer_1.timeout.connect(self.update_plot_1)
+        self.play_button_1.clicked.connect(self.play_pause_1)
+        self.speed_button_1.clicked.connect(self.change_speed_1)
+        self.clear_button_1.clicked.connect(self.clear_1)
+        self.actionPlay_Pause.triggered.connect(self.play_pause_1)
         self.channel1_slider.valueChanged.connect(self.on_channel_1_slider_change)
+
+        # Channel 2
+        self.import_signal_ch2.triggered.connect(self.import_signal_channel_2) 
+        self.timer_2.timeout.connect(self.update_plot_2)
+        self.play_button_2.clicked.connect(self.play_pause_2)
+        self.speed_button_2.clicked.connect(self.change_speed_2)
+        self.clear_button_2.clicked.connect(self.clear_2)
+        self.actionPlay_Pause_2.triggered.connect(self.play_pause_2)
+        self.channel2_slider.valueChanged.connect(self.on_channel_2_slider_change)
 
     def on_channel_1_slider_change(self, value):
         self.widget.setXRange(value - 1, value)
 
+    def on_channel_2_slider_change(self, value):
+        self.widget_2.setXRange(value - 1, value)
 
     def import_signal_channel_1(self):
-        signal: Signal = self.get_signal_from_file()
+        signal: Signal = get_signal_from_file(self)
         self.render_signal_to_channel_1(channel=self.widget, signal=signal)
     
     def import_signal_channel_2(self):
-        signal: Signal = self.get_signal_from_file()
+        signal: Signal = get_signal_from_file(self)
         self.render_signal_to_channel_2(channel=self.widget_2, signal=signal)
-
-    def get_signal_from_file(self):
-        # get path of signal files only of types (xls, csv, txt)
-        file_path, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Single File', QtCore.QDir.rootPath(), "(*.xls);;(*.txt);;(*.csv);;(*.xlsx)")
-
-        # check the type of signal file
-        file_type = file_path.split('.')[-1]
-
-        # Picking the right loader from file_type
-        loader: ISignalLoader
-        if file_type == 'xls':
-            loader = ExcelSignalLoader()
-        elif file_type == 'xlsx':
-            loader = ExcelXSignalLoader()
-        elif file_type == 'csv':
-            loader = CSVSignalLoader()
-        else:
-            loader = TextSignalLoader()
-        
-        signal: Signal = loader.load(file_path)
-        return signal
 
     def render_signal_to_channel_1(self, channel, signal):
         # Set up the initial plot
@@ -99,7 +82,6 @@ class MainWindow(uiclass, baseclass):
         _, x_limit_max = curve_1.dataBounds(0)
         self.channel1_slider.setMinimum(1)
         self.channel1_slider.setMaximum(int(x_limit_max))
-        
 
     def render_signal_to_channel_2(self, channel, signal):
         # Set up the initial plot
@@ -114,7 +96,12 @@ class MainWindow(uiclass, baseclass):
 
         # Fix the y-limit (prevent auto vertical zooming)
         y_limit_min, y_limit_max = curve_2.dataBounds(1)
-        self.widget.setYRange(y_limit_min, y_limit_max)
+        self.widget_2.setYRange(y_limit_min, y_limit_max)
+
+        # Initialize the slider with the right values
+        _, x_limit_max = curve_2.dataBounds(0)
+        self.channel2_slider.setMinimum(1)
+        self.channel2_slider.setMaximum(int(x_limit_max))
 
     def update_plot_1(self):
        if self.is_plotting_1:
@@ -125,7 +112,6 @@ class MainWindow(uiclass, baseclass):
                     self.widget.setXRange(0, 1)
                 else:    
                     self.widget.setXRange(x_data[-1]-1, x_data[-1])
-                    print(x_data[-1])
                 self.channel1_slider.setValue(int(x_data[-1]))
                 self.channel1_slider.repaint()
                 self.curve_1.setData(x_data, y_data)
@@ -144,6 +130,8 @@ class MainWindow(uiclass, baseclass):
                     self.widget_2.setXRange(0, 1)
                 else:    
                     self.widget_2.setXRange(x_data[-1]-1, x_data[-1])
+                self.channel2_slider.setValue(int(x_data[-1]))
+                self.channel2_slider.repaint()
                 self.curve_2.setData(x_data, y_data)
                 self.data_index_2 += 1
             else:
@@ -219,7 +207,6 @@ def main():
     window = MainWindow()
     window.show()
     app.exec()
-
 
 if __name__ == '__main__':
     main()
